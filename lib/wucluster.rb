d5f03be33f6303c61e730c85243c0f543ebaa4f9
Philip (flip) Kromer <flip@infochimps.org>
2010-01-16 04:13:27 -0600

    writing wucluster script

--- lib/wucluster.rb
+++ lib/wucluster.rb
@@ -1,52 +1,30 @@
 $: << File.dirname(__FILE__)
-require 'json'
-require 'yaml'
-require 'active_support'
-# require 'wukong/extensions'
-# require 'wukong/extensions/hash_keys'
-require 'AWS'
-require File.dirname(__FILE__)+'/AWS/EC2/snapshot'
-require File.dirname(__FILE__)+'/AWS/EC2/mock'
-require 'wukong/logger'
-require 'wucluster/mock'
+require 'configliere'; Configliere.use :define
+# require 'AWS'
+# require File.dirname(__FILE__)+'/AWS/EC2/snapshot'
+# require File.dirname(__FILE__)+'/AWS/EC2/mock'
+# require 'wukong/logger'
+# require 'wucluster/mock'
 
 module Wucluster
-  CONFIG_DIR = ENV['HOME']+'/.hadoop-ec2'
-  MAX_TRIES  = 14
-  SLEEP_TIME = 0.5 unless defined?(SLEEP_TIME)
+  Settings.define :sleep_time, :type => Float,   :default => 1.0, :description => "How long to sleep between attempts"
+  Settings.define :max_tries,  :type => Integer, :default => 15,  :description => "How many times to attempt an operation before giving up"
+  Settings.define :access_key_id,                :required => true, :description => "Amazon AWS access key ID, found in your AWS console (http://bit.ly/awsconsole)"
+  Settings.define :secret_access_key,            :required => true, :description => "Amazon AWS secret access key, found in your AWS console (http://bit.ly/awsconsole)"
 
-  autoload :Cluster,     'wucluster/cluster'
-  autoload :Mount,       'wucluster/mount'
-  autoload :Node,        'wucluster/node'
-  autoload :Ec2Volume,   'wucluster/ec2_volume'
-  autoload :Ec2Instance, 'wucluster/ec2_instance'
-  autoload :Ec2Snapshot, 'wucluster/ec2_snapshot'
-  require 'wucluster/mock_cluster_mount'
+  # autoload :Cluster,     'wucluster/cluster'
+  # autoload :Mount,       'wucluster/mount'
+  # autoload :Node,        'wucluster/node'
+  # autoload :Ec2Volume,   'wucluster/ec2_volume'
+  # autoload :Ec2Instance, 'wucluster/ec2_instance'
+  # autoload :Ec2Snapshot, 'wucluster/ec2_snapshot'
+  # require 'wucluster/mock_cluster_mount'
 
   #
   # single point of access to AWS calls
   #
   def self.ec2
-    @ec2 ||= AWS::EC2::Base.new( :access_key_id => access_key_id, :secret_access_key => secret_access_key)
-  end
-
-  #
-  # Configuration -- uses ~/.hadoop-ec2/s3config.yml (and not env. vars)
-  #
-  def self.config
-    @config ||= YAML.load(File.open(Wucluster::CONFIG_DIR+'/s3config.yml')).symbolize_keys!
-  end
-  # the aws access_key_id, taken from the global config.
-  def self.access_key_id
-    config[:aws_access_key_id]
-  end
-  # the aws secret_access_key, taken from the global config.
-  def self.secret_access_key
-    config[:aws_secret_access_key]
-  end
-  # the aws account_id, taken from the global config
-  def self.aws_account_id
-    config[:aws_account_id]
+    @ec2 ||= AWS::EC2::Base.new(:access_key_id => Settings.access_key_id, :secret_access_key => Settings.secret_access_key)
   end
 
 end
